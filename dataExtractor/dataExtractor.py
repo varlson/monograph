298df9f183968850cba82638dcffe8903ca65c20
Suleimane Ducure <suleimane.ducure@aluno.ufop.edu.br>
2023-12-23 09:56:26 -0300

    sharing

--- dataExtractor/dataExtractor.py
+++ dataExtractor/dataExtractor.py
@@ -1,4 +1,27 @@
-import pandas as pd
-def dataFilter(pathName, graph):
-    dataFrame = pd.read_csv(pathName)
-    
+# import pandas as pd
+import igraph as ig
+
+
+def dataFilter (graph, output):
+    g = graph.copy()
+    del g.vs['LATI']
+    del g.vs['LONG']
+
+    x = graph.vs['LONG']
+    y = graph.vs['LATI']
+
+    y = [(-1)* current for current in y]
+
+    g.vs['x'] = x
+    g.vs['y'] = y
+    g.vs['name'] = graph.vs['NOMEMUN']
+    del g.vs['NOMEMUN']
+
+    g.write_graphml(output)
+
+
+
+
+# g = ig.Graph.Read_GraphML('networkBuilder/input/grafo_peso_rodo.GraphML')
+g = ig.Graph.Read_GraphML('networkBuilder/input/grafo_peso_hidro.GraphML')
+dataFilter(g, "networkBuilder/input/filtred/fluvial.GraphML")
